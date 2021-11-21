0e373a2b772948009b1a0ffc6241534ad142ec82
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-21 10:35:35 -0500

    21.11-dev-13

--- src/views/index.php
+++ src/views/index.php
@@ -1,3 +1,4 @@
-<div class="motd-content-wrapper motd-background">
-  <span class="align-middle">Bienvenue au mariage de Louis Ouellet et Christelle Tsague</span>
+<div class="motd-content-wrapper motd-background align-middle text-center">
+  <!-- <span class="align-middle">Bienvenue au mariage de Louis Ouellet et Christelle Tsague</span> -->
+  Bienvenue au mariage de Louis Ouellet et Christelle Tsague
 </div>
