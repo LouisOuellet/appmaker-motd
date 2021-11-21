9fe4fff701dbd437762749d1dc9ce90bb0652f23
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-21 09:44:01 -0500

    21.11-dev-5

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,11 @@ API.Plugins.motd = {
 	init:function(){
 		API.GUI.Sidebar.Nav.add('motd', 'main_navigation');
 	},
+	load:{
+		index:function(){
+			$('.content-header').hide();
+		},
+	},
 }
 
 API.Plugins.motd.init();
