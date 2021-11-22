2bee72d092f1f97631b626d98ff0b2670023f7cb
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-22 11:56:27 -0500

    21.11-dev-17

--- src/views/index.php
+++ src/views/index.php
@@ -1,9 +1,9 @@
 <link rel="preload" href="plugins/motd/dist/fonts/Scriptina.woff2" as="font" type="font/woff2" crossorigin>
 <div class="motd-content-wrapper motd-background row align-items-center text-center">
-  <div class="col motd-box">
+  <div class="col motd-box bg-dark">
     <p>Bienvenue au mariage de</p>
-    <p style="font-family: 'Scriptina'">Louis Ouellet</p>
-    <p style="font-family: 'Scriptina'">et</p>
-    <p style="font-family: 'Scriptina'">Christelle Tsague</p>
+    <p style="font-family: 'Scriptina'"><h1>Louis Ouellet</h1></p>
+    <p style="font-family: 'Scriptina'"><h1>et</h1></p>
+    <p style="font-family: 'Scriptina'"><h1>Christelle Tsague</h1></p>
   </div>
 </div>
