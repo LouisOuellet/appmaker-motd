3f8ba2ec341c8a43bdcb22fb7154b959c7805298
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-22 11:51:28 -0500

    21.11-dev-16

--- src/views/index.php
+++ src/views/index.php
@@ -1,4 +1,9 @@
-<div class="motd-content-wrapper motd-background text-center">
-  <span class="align-middle">Bienvenue au mariage de Louis Ouellet et Christelle Tsague</span>
-  <!-- Bienvenue au mariage de Louis Ouellet et Christelle Tsague -->
+<link rel="preload" href="plugins/motd/dist/fonts/Scriptina.woff2" as="font" type="font/woff2" crossorigin>
+<div class="motd-content-wrapper motd-background row align-items-center text-center">
+  <div class="col motd-box">
+    <p>Bienvenue au mariage de</p>
+    <p style="font-family: 'Scriptina'">Louis Ouellet</p>
+    <p style="font-family: 'Scriptina'">et</p>
+    <p style="font-family: 'Scriptina'">Christelle Tsague</p>
+  </div>
 </div>
