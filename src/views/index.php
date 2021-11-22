ad4affee133b4bb257b0740294cd2997a34fdccf
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-22 12:16:42 -0500

    21.11-dev-22

--- src/views/index.php
+++ src/views/index.php
@@ -2,7 +2,7 @@
 <link rel="preload" href="plugins/motd/dist/fonts/SignPainterHouseScript.woff2" as="font" type="font/woff2" crossorigin>
 <div class="motd-content-wrapper motd-background row align-items-center text-center">
   <div class="d-flex justify-content-center">
-    <div class="w-auto d-inline-flex motd-box bg-black">
+    <div class="w-auto motd-box bg-black">
       <p>Bienvenue au mariage de</p>
       <p><h1>Louis Ouellet</h1></p>
       <p><h1>et</h1></p>
