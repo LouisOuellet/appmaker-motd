a84962db82a8e60b442c4e9cc1ac84cb648b9240
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-23 09:54:46 -0500

    21.11-dev-32

--- src/views/index.php
+++ src/views/index.php
@@ -2,10 +2,10 @@
 <link rel="preload" href="plugins/motd/dist/fonts/SignPainterHouseScript.woff2" as="font" type="font/woff2" crossorigin>
 <div class="motd-content-wrapper motd-background row align-items-center text-center justify-content-center">
   <div class="w-auto motd-box bg-black noselect">
-    <p>Bienvenue au mariage de</p>
+    <p><h2>Bienvenue au mariage de</h2></p>
     <p><h1>Louis Ouellet</h1></p>
     <p><h1>et</h1></p>
     <p><h1>Christelle Tsague</h1></p>
-    <button class="btn btn-warning btn-lg">Entrer</button>
+    <p><button class="btn btn-warning btn-lg">Entrer</button></p>
   </div>
 </div>
