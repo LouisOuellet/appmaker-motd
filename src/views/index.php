19a428be380c596c0878d31c16f78bea3fcf3c98
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-23 10:20:52 -0500

    21.11-dev-34

--- src/views/index.php
+++ src/views/index.php
@@ -3,7 +3,7 @@
 <div class="motd-content-wrapper motd-background row align-items-center text-center justify-content-center">
   <div class="w-auto motd-box bg-black noselect">
     <p><h2>Bienvenue au mariage de</h2></p>
-    <p><h1>Louis Ouellet</h1></p>
+    <p class="p-0"><h1>Louis Ouellet</h1></p>
     <p><h1>et</h1></p>
     <p><h1>Christelle Tsague</h1></p>
     <p class="p-0 pt-3"><button class="btn btn-warning btn-lg">Entrer</button></p>
