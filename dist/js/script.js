1378d298f42f6a7f8ddddfbd536ebc5ef0e452f9
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-23 11:46:37 -0500

    21.11-dev-46

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,6 +4,20 @@ API.Plugins.motd = {
 	},
 	load:{
 		index:function(){
+			var html = '';
+			html += '<div class="motd-content-wrapper motd-background row align-items-center text-center justify-content-center">';
+			  html += '<div class="w-auto motd-box bg-black noselect" id="motd-1">';
+			    html += '<p><h2>Bienvenue au mariage de</h2></p>';
+			    html += '<p><h1 class="mt-3">Louis Ouellet</h1></p>';
+			    html += '<p><h1>et</h1></p>';
+			    html += '<p class="mt-4"><h1 class="mt-4">Christelle Tsague</h1></p>';
+			    html += '<p class="mt-4"><button class="btn btn-warning btn-lg mt-4">Entrer</button></p>';
+			  html += '</div>';
+			  html += '<div class="w-auto motd-box bg-black noselect hide" id="motd-2">';
+			    html += '<p><button class="btn btn-warning btn-lg">Continuer</button></p>';
+			  html += '</div>';
+			html += '</div>';
+			$('body').prepend(html);
 			$('#motd-1 button').off().click(function(){
 				$('#motd-1').fadeOut('slow','swing',function(){
 					$('#motd-2').fadeIn('slow','swing');
--- src/views/index.php
+++ src/views/index.php
@@ -1,14 +1,2 @@
 <link rel="preload" href="plugins/motd/dist/fonts/Scriptina.woff2" as="font" type="font/woff2" crossorigin>
 <link rel="preload" href="plugins/motd/dist/fonts/SignPainterHouseScript.woff2" as="font" type="font/woff2" crossorigin>
-<div class="motd-content-wrapper motd-background row align-items-center text-center justify-content-center">
-  <div class="w-auto motd-box bg-black noselect" id="motd-1">
-    <p><h2>Bienvenue au mariage de</h2></p>
-    <p><h1 class="mt-3">Louis Ouellet</h1></p>
-    <p><h1>et</h1></p>
-    <p class="mt-4"><h1 class="mt-4">Christelle Tsague</h1></p>
-    <p class="mt-4"><button class="btn btn-warning btn-lg mt-4">Entrer</button></p>
-  </div>
-  <div class="w-auto motd-box bg-black noselect hide" id="motd-2">
-    <p><button class="btn btn-warning btn-lg">Continuer</button></p>
-  </div>
-</div>
