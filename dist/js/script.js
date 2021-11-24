d3e662fb54bdb2c22fc6ffe5cc0b374aa20bf68e
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-24 12:43:18 -0500

    21.11-dev-68

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,21 +21,48 @@ API.Plugins.motd = {
 					  html += '</button>';
 					  html += '<div class="collapse navbar-collapse" id="navbarMOTD">';
 					    html += '<div class="navbar-nav">';
-					      html += '<a class="nav-item nav-link active" data-page="A Propos">A Propos</a>';
-					      html += '<a class="nav-item nav-link" data-page="Gallerie">Gallerie</a>';
-					      html += '<a class="nav-item nav-link" data-page="Invitations">Invitations</a>';
-					      html += '<a class="nav-item nav-link" data-page="Programme">Programme</a>';
-					      html += '<a class="nav-item nav-link" data-page="Menu">Menu</a>';
+					      html += '<a class="nav-item nav-link active" data-page="about">A Propos</a>';
+					      html += '<a class="nav-item nav-link" data-page="gallery">Gallerie</a>';
+					      html += '<a class="nav-item nav-link" data-page="attendance">Invitations</a>';
+					      html += '<a class="nav-item nav-link" data-page="planning">Programme</a>';
+					      html += '<a class="nav-item nav-link" data-page="menu">Menu</a>';
 					    html += '</div>';
 					  html += '</div>';
 					html += '</nav>';
+					html += '<div class="motd-pages">';
+						html += '<div class="motd-page" data-page="about">';
+							html += '<p><h2>A Propos</h2></p>';
+							html += '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
+							html += '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>';
+						html += '</div>';
+						html += '<div class="motd-page hide" data-page="gallery">';
+							html += '<p><h2>Gallerie</h2></p>';
+							html += '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
+							html += '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>';
+						html += '</div>';
+						html += '<div class="motd-page hide" data-page="attendance">';
+							html += '<p><h2>Invitations</h2></p>';
+							html += '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
+							html += '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>';
+						html += '</div>';
+						html += '<div class="motd-page hide" data-page="planning">';
+							html += '<p><h2>Programme</h2></p>';
+							html += '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
+							html += '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>';
+						html += '</div>';
+						html += '<div class="motd-page hide" data-page="menu">';
+							html += '<p><h2>Menu</h2></p>';
+							html += '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>';
+							html += '<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>';
+						html += '</div>';
+					html += '</div>';
 			    html += '<p><button class="btn btn-warning btn-lg" data-action="ControlPanel">Control Panel</button></p>';
 			  html += '</div>';
 			html += '</div>';
 			$('body').prepend(html);
 			var motd = $('body').find('div.motd-content-wrapper').first();
 			var nav = motd.find('nav.navbar.navbar-expand-lg.navbar-dark.bg-transparent').first();
-			console.log(motd,nav);
+			var pages = motd.find('div.motd-pages').first();
 			$('#motd-1 button').off().click(function(){
 				$('#motd-1').fadeOut('slow','swing',function(){
 					$('#motd-2').fadeIn('slow','swing');
@@ -46,8 +73,12 @@ API.Plugins.motd = {
 				$('div.motd-content-wrapper').fadeOut('slow','swing');
 			});
 			nav.find('a[data-page]').off().click(function(){
+				var page = $(this).attr('data-page');
 				nav.find('a[data-page]').removeClass('active');
 				$(this).addClass('active');
+				pages.find('div[data-page]').fadeOut('slow','swing',function(){
+					pages.find('div[data-page="'+page+'"]').fadeIn('slow','swing');
+				});
 			});
 		},
 	},
