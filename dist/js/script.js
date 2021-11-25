24400342c13c8288425541842fbc1ae15d526659
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-24 22:31:14 -0500

    21.11-dev-145

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,6 +19,9 @@ API.Plugins.motd = {
 					  html += '<div class="w-auto motd-box bg-black noselect" id="motd-1">';
 					    html += '<p><h2>Bienvenue au mariage de</h2></p>';
 							for(var [id, host] of Object.entries(API.Helper.trim(data.this.raw.setHosts,';').split(';'))){
+								console.log(data.relations[data.this.raw.setHostType]);
+								console.log(id);
+								console.log(data.relations[data.this.raw.setHostType][id]);
 								if(API.Helper.isSet(data,['relations',data.this.raw.setHostType,id])){
 									if(count > 0){ html += '<p><h1>&</h1></p>'; }
 									html += '<p><h1 class="mt-3">'+data.relations[data.this.raw.setHostType][id].name+'</h1></p>';
